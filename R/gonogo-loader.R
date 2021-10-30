34787cab230afa5c4c20594ae22f7cf5b77e175a
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-10-30 16:44:21 +0200

    Simplifies the functions and the loading pipeline, moves functions to proper places

--- R/gonogo-loader.R
+++ R/gonogo-loader.R
@@ -3,17 +3,24 @@
 #' @param pth 
 #' @param skip_first_section some files ahve multiple starting points. if true,
 #' we simply skip to the last portion
+#' @param preprocess shoul dthe data be automatically processed. Calls `preprocess_experiment` function
 #'
-#' @return
+#' @return data.frame
 #' @export
 #'
 #' @examples
-load_experiment <- function(pth, skip_first_section = TRUE){
+load_experiment <- function(pth, skip_first_section = TRUE, preprocess = TRUE){
   if(skip_first_section){
-    i_head <- grep("subnum,block,type,correctresponse,trial,choice,x,y,stim,present,response,responded,starttime,rt",
-         readLines(pth), value = FALSE)
+    #ptr <- "subnum,block,type,correctresponse,trial,choice,x,y,stim,present,response,responded,starttime,rt"
+    ptr <- "subnum,block,type,correctresponse.*"
+    i_head <- grep(ptr, readLines(pth,100), value = FALSE)
     i_head <- i_head[length(i_head)] - 1
+  } else {
+    i_head <- 0
   }
   df_data <- read.table(pth, header = TRUE, sep = ",", skip = i_head)
+  if(preprocess){
+    df_data <- preprocess_experiment(df_data)
+  }
   return(df_data)
 }
--- R/gonogo-preprocessing.R
+++ R/gonogo-preprocessing.R
@@ -1,24 +1,40 @@
+#' Prepares experimental data to cleaner stats
+#' 
+#' @description Renames columns, recodes some values, adds new deduced values
+#'
+#' @param df_experiment 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 preprocess_experiment <- function(df_experiment){
-  colnames(df_experiment) <- c("subject", "block", "trial_type", "go_response",
-                               "trial", "choice", "x", "y", "stimulus_shown",
-                               "should_go", "key_response", "response_correct",
-                               "start_time", "reaction_time")
+  oldnames <- c("subnum","block","type","correctresponse",
+                "trial","choice","x","y","stim","present",
+                "response","responded", "starttime",
+                "corr", "rt")
+  newnames <- c("subject", "block", "trial_type", "go_response",
+                "trial", "choice", "x", "y", "stimulus_shown",
+                "should_go", "key_response", "response_correct",
+                "start_time", "corr", "reaction_time")
+  df_experiment <- col_rename(df_experiment, oldnames, newnames)
   df_experiment$responded <- df_experiment$key_response != "NONE"
   df_experiment$should_go <- df_experiment$should_go == 1
   df_experiment$response_correct <- df_experiment$responded == df_experiment$should_go
   return(df_experiment)   
 }
 
-#' Extracts subject from preprocessed data and returns it
-#'
-#' @param df_experiemnt 
-#'
-#' @return
-#' @export
-#'
-#' @examples
-extract_subject <- function(df_experiemnt){
-  subj <- df_experiemnt$subject[1]
-  df_experiemnt$subject <- NULL
-  return(list(subject = subj, data = df_experiemnt))
+col_rename <- function(df, oldnames, newnames){
+  if(length(oldnames) != length(newnames)){
+    warning("Column lenghts do not correspond")
+    return(df)
+  }
+  for(i in seq_len(length(oldnames))){
+    i_col <- which(colnames(df) == oldnames[i])
+    if(length(i_col) < 1) next 
+    colnames(df)[i_col] <- newnames[i]
+  }
+  return(df)
 }
+
+
